ccfdbe7487be30dc34e8ac4bf79c5258617cfa82
saragomez98 <102761696+saragomez98@users.noreply.github.com>
2022-06-14 22:01:53 -0500

    Update question.sh

--- question.sh
+++ question.sh
@@ -50,6 +50,60 @@ sed 's-n-\\N-g' paso5>paso6
 sed 's/\([0-9][0-9]\)\/\([0-9][0-9]\)\/\([0-9][0-9]\)/20\3-\2-\1/g' paso6>paso7  
 sed 's/\([0-9]\)\/\([0-9]\)\/\([0-9][0-9]*\)/\3-0\2-0\1/g' paso7>paso8
 sed 's/,\\N,\\N,/,\\N,\\N,\\N/g' paso8>paso9
-sed 's/,C,\\N,/,C,\\N,\\N/g' paso9>output.csv
+#
+#  LAB: Transformación de archivos con `sed`
+#  ================================================
+#
+#  Una tarea común en Analytics es el procesamiento de archivos en bruto para que puedan 
+#  ser usados en otros procesos, donde gran parte de dicho procesamiento corresponde a la 
+#  transforamción del formato. Estos archivos suelen tener millones de registros por lo que 
+#  la aplicación de procedimientos manuales no es práctica.
+#  
+#  El archivo `data.csv` contiene una muestra de los archivos generados automáticamente 
+#  por un software. El archivo contiene cuatro (4) columnas, donde los valores válidos
+#  para los campos son los siguientes:
+#  
+#  * Columna 1: `DD/MM/AA`.
+#  * Columna 2: `A`, `B`, `C`. `\N` indica NULL.
+#  * Columna 3: Número entero. `\N` indica NULL.
+#  * Columna 4: Número decimal. `\N` indica NULL.
+#  
+#  
+#  Usted debe escribir un script en bash que use sed para que realice 
+#  las siguientes acciones:
+#    
+#  * Convierta el formato de las fechas de DD/MM/YY a YYYY-MM-DD.
+#  
+#  * Transforme el archivo para que todos los campos nulos aparezcan como `\N`.
+#  
+#  * Reemplace los `;` por `,`.
+#  
+#  * Use el `.` para indicar decimales.
+#
+#  El resultado del script se escribe en pantalla.
+#
+#  El programa se llamara por el evaluador tal como se indica a continuación:
+#
+#  $ bash question.sh data.csv > output.csv
+#  
+#  Rta/
+#  2013-03-12,A,1,100.0
+#  ...
+#  2014-09-01,A,3,100.4
+#
+#  >>> Escriba su codigo a partir de este punto <<<
+#
+sed 's/,\([0-9]\)/.\1/' data.csv > paso1
+sed 's/;/,/g' paso1>paso2
+sed 's-\\n-\\N-g' paso2>paso3
+sed 's-,N-,\\N-g' paso3>paso4
+sed 's-,,-,\\N,-g' paso4>paso5  
+sed 's-n-\\N-g' paso5>paso6
+sed 's/\([0-9][0-9]\)\/\([0-9][0-9]\)\/\([0-9][0-9]\)/20\3-\2-\1/g' paso6>paso7  
+sed 's/\([0-9]\)\/\([0-9]\)\/\([0-9][0-9]*\)/\3-0\2-0\1/g' paso7>paso8
+sed 's/,\\N,\\N,/,\\N,\\N,\\N/g' paso8>paso9
+sed 's/,C,\\N,/,C,\\N,\\N/g' paso9>paso10
+sed 's/a/A/g' paso10>paso11
+sed 's/c/C/g' paso11>output.csv
 sed -n '1,12p' output.csv
 
